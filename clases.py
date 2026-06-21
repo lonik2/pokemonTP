1b63177fbf907d5ae676be5863934ca2f62a9658
Lucas <lonyszczyk@alumno.huergo.edu.ar>
2026-06-21 12:16:50 -0300

    pokedex terminada

--- clases.py
+++ clases.py
@@ -1,37 +1,50 @@
 class HashMap:
-    def __init__(self, tamaño=20):
+    def __init__(self, tamaño=17):
         self.tamaño = tamaño
-        self.buckets = [[] for _ in range(tamaño)]
+        self.buckets = [None for _ in range(tamaño)]
 
     def hash_funcion(self, key):
         suma_num = sum(int(char) for char in key if char.isdigit())
-        return suma_num % 10
+        return suma_num % self.tamaño
 
     def agregar(self, key, value):
         indice = self.hash_funcion(key)
-        bucket = self.buckets[indice]
-        for i, (k, v) in enumerate(bucket):
+        intentos = 0
+        while self.buckets[indice] is not None and intentos < self.tamaño:
+            k, v = self.buckets[indice]
             if k == key:
-                bucket[i] = (key, value)
+                self.buckets[indice] = (key, value)
                 return
-        bucket.append((key, value))
+            indice = (indice + 1) % self.tamaño
+            intentos += 1
+        if intentos < self.tamaño:
+            self.buckets[indice] = (key, value)
 
     def buscar(self, key):
         indice = self.hash_funcion(key)
-        bucket = self.buckets[indice]
-        for k, v in bucket:
+        intentos = 0
+        while self.buckets[indice] is not None and intentos < self.tamaño:
+            k, v = self.buckets[indice]
             if k == key:
                 return v
+            indice = (indice + 1) % self.tamaño
+            intentos += 1
         return None
 
     def borrar(self, key):
         indice = self.hash_funcion(key)
-        bucket = self.buckets[indice]
-        for i, (k, v) in enumerate(bucket):
+        intentos = 0
+        while self.buckets[indice] is not None and intentos < self.tamaño:
+            k, v = self.buckets[indice]
             if k == key:
-                del bucket[i]
+                self.buckets[indice] = None
                 return
+            indice = (indice + 1) % self.tamaño
+            intentos += 1
 
     def mapa(self):
+        contador = 0
         for indice, bucket in enumerate(self.buckets):
-            print(f"Bucket {indice}: {bucket}")
+            contador+= 1
+            print(contador, bucket)
+
--- pokemon.py
+++ pokemon.py
@@ -8,8 +8,24 @@ class Pokemon:
         self.nombre = nombre
         self.tipo = tipo
         self.poder_combate = poder_combate
+    
+    def __repr__(self):
+        return f"{self.nombre} ({self.tipo}) ({self.poder_combate})"
 
 
 Pokedex = HashMap()
 
-Pokedex.agregar ()
+try:
+    with open("pokemones.json", "r", encoding="utf-8") as pokemones:
+        lista_pokemones = json.load(pokemones)
+
+    for p in lista_pokemones:
+        datos_pokemon = Pokemon(p["id"], p["nombre"], p["tipo"], p["poder_combate"])
+        clave = str(p["id"])
+        Pokedex.agregar(clave, datos_pokemon)
+except FileNotFoundError:
+    print ("no se encontro el json con los datos")
+
+
+print ("-----POKEDEX NACIONAL-----")
+Pokedex.mapa()
